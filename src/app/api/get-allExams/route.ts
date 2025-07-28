c310e0c736b7b1e16ed1d8c636de1a62828387e9
MohamedAli <mmkandeelz74@gmail.com>
2025-07-28 03:46:22 +0300

    edit

--- src/app/api/get-allExams/route.ts
+++ src/app/api/get-allExams/route.ts
@@ -4,10 +4,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const token = await getToken({ req });
-
+  if (!token?.token) {
+    return NextResponse.json(
+      { message: "Unauthorized: token not provided" },
+      { status: 401 }
+    );
+  }
   const respone = await fetch(`${process.env.API}/exams`, {
     headers: {
-      token: token?.token || "",
+      token: token?.token,
       ...JSON_HEADER,
     },
   });
