4eb95b4ec623c530346d6cbbfa5721b413371670
MohamedAli <mmkandeelz74@gmail.com>
2025-08-29 05:37:48 +0300

    edit

--- src/app/[locale]/auth/layout.tsx
+++ src/app/[locale]/auth/layout.tsx
@@ -4,11 +4,11 @@ import { Toaster } from "@/components/ui/sonner";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="grid grid-cols-1 lg:grid-cols-2 h-screen w-full">
+    <main className="grid grid-cols-1 lg:grid-cols-2 w-full">
       {/* Welcome section */}
       <Welcome />
       <section className="lg:col-span-1">
-        <div className="  flex items-center justify-center p-10 h-full ">
+        <div className="flex items-center justify-center p-10 h-full">
           {/* form */}
           {children}
           <Toaster />
